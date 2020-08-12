46024ef590d93a087f4f0cea8f03ea16418c91e9
suchangqin <suchangqin@gmail.com>
2020-08-12 11:52:45 +0800

    changes

--- test/concurrent_test.go
+++ test/concurrent_test.go
@@ -9,29 +9,34 @@ import (
 	ultipa "ultipa-go-sdk/rpc"
 	"ultipa-go-sdk/utils"
 )
-var CONCURRENT_COUNT = 100000
+var host = "124.193.211.21:60062"
+var graphSetName = "default"
+
+var CONCURRENT_COUNT = 10000
+func uql_build(index int) string  {
+	uql := fmt.Sprintf("find().nodes(%d)", rand.Intn(CONCURRENT_COUNT*2))
+	return uql
+}
 
 func TestConcurrent(t *testing.T) {
-	host := "124.193.211.21:60062"
 	conn, _ := GetTestDefaultConnection(&host)
-	conn.DefaultConfig.GraphSetName = "default"
+	conn.DefaultConfig.GraphSetName = graphSetName
 	conn.RefreshRaftLeader("", nil)
 
 	var wg sync.WaitGroup
 	timeStart := time.Now().UnixNano()
 	for i:=1;i<CONCURRENT_COUNT;i++ {
-		go func() {
+		go func(index int) {
 			wg.Add(1)
 			defer wg.Done()
-			uql := fmt.Sprintf("find().nodes(%d)", rand.Intn(CONCURRENT_COUNT*2))
+			uql := uql_build(index)
 			res := conn.UQL(uql, nil)
-
 			if res.Status.Code == ultipa.ErrorCode_SUCCESS {
 				//println(uql)
 			} else {
 				fmt.Printf("%s, %s", uql, utils.JSONString(res))
 			}
-		}()
+		}(i)
 	}
 	wg.Wait()
 	timeEnd := time.Now().UnixNano()
