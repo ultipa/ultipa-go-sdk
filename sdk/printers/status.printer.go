ec0e4dd4019f63b4cb51bdbd9e8d03c5940166a9
zhangjiansong <jiansong@ultipa.com>
2022-06-15 10:16:29 +0800

    update printer

---
+++ sdk/printers/status.printer.go
@@ -0,0 +1,24 @@
+package printers
+
+import (
+	"fmt"
+	"ultipa-go-sdk/sdk/http"
+)
+
+func PrintStatistics(stats *http.Statistic) {
+
+	tc := stats.TotalCost
+	ec := stats.EngineCost
+	ne := stats.NodeAffected
+	ee := stats.EdgeAffected
+
+	if ne != 0 {
+		fmt.Printf("Effect Nodes: %v \n", ne)
+	}
+
+	if ee != 0 {
+		fmt.Printf("Effect Edges: %v \n", ee)
+	}
+
+	fmt.Printf("Total Cost : %vs | Engine Cost : %vs \n", float64(tc) / 1000, float64(ec) / 1000 )
+}
