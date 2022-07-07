6630c44692fd77d259ec32c673dd34d18ceff6b5
zhaohaichao <810871100@qq.com>
2022-07-07 20:00:05 +0800

    explain follow up

---
+++ sdk/http/explainPlan.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	ultipa "ultipa-go-sdk/rpc"
+	"ultipa-go-sdk/sdk/structs"
+)
+
+type ExplainPlan struct {
+	Explain []*structs.Explain
+}
+
+func ParseExplainPlan(ex *ultipa.ExplainPlan) (*ExplainPlan, error) {
+	explainPlan := ExplainPlan{
+		Explain: []*structs.Explain{},
+	}
+
+	if ex == nil {
+		return &explainPlan, nil
+	}
+
+	for _, planNode := range ex.PlanNodes {
+		explain := structs.Explain{
+			Type:        planNode.GetType(),
+			Alias:       planNode.GetAlias(),
+			ChildrenNum: planNode.GetChildrenNum(),
+			Uql:         planNode.GetUql(),
+			Infos:       planNode.GetInfos(),
+		}
+		explainPlan.Explain = append(explainPlan.Explain, &explain)
+	}
+
+	return &explainPlan, nil
+}
