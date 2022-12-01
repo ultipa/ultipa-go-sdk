b8a05b87b7b65476507caede1ed71c089b057f17
luzuoqi <luzuoqi@ultipa.com>
2022-12-01 17:11:06 +0800

    download file

---
+++ sdk/api/download.file.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"io"
+	ultipa "ultipa-go-sdk/rpc"
+	"ultipa-go-sdk/sdk/configuration"
+)
+
+func (api *UltipaAPI) DownloadFileV2(fileName string, taskId uint64, config *configuration.RequestConfig, receive func(data []byte) error) error {
+	var err error
+
+	client, err := api.GetControlClient(config)
+
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := api.Pool.NewContext(config)
+	defer cancel()
+
+	resp, err := client.DownloadFileV2(ctx, &ultipa.DownloadFileRequestV2{
+		FileName: fileName,
+		TaskId:   taskId,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		record, err := resp.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		err = receive(record.Chunk)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return err
+}
