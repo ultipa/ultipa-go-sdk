c946f570a86ffa5eda630c86a6bf559ac27933d6
luzuoqi <luzuoqi@ultipa.com>
2023-02-21 10:51:35 +0800

    remove totalEdges from showNodeSchema test

--- sdk/api/schema.go
+++ sdk/api/schema.go
@@ -32,14 +32,14 @@ func (api *UltipaAPI) ListNodeSchema(config *configuration.RequestConfig) (*http
 	values := table.ToKV()
 	for _, v := range values {
 		totalNodes, _ := strconv.ParseInt(v.Get("totalNodes").(string), 10, 64)
-		totalEdges, _ := strconv.ParseInt(v.Get("totalEdges").(string), 10, 64)
+		//totalEdges, _ := strconv.ParseInt(v.Get("totalEdges").(string), 10, 64)
 
 		schemas = append(schemas, &http.ResponseSchema{
 			Name:        v.Get("name").(string),
 			Description: v.Get("description").(string),
 			Properties:  nil,
 			TotalNodes:  totalNodes,
-			TotalEdges:  totalEdges,
+			//TotalEdges:  totalEdges,
 		})
 	}
 	return &http.ResponseNodeSchemas{
