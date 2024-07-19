bf798147ee6b690c0a2d82e8d3df0de19b6ecb7b
yujincheng <272167037@qq.com>
2024-07-19 14:44:03 +0800

    downalgofile

--- sdk/api/download.file.go
+++ sdk/api/download.file.go
@@ -45,10 +45,6 @@ func (api *UltipaAPI) DownloadAlgoResultFile(fileName string, taskId string, con
 		}
 	}
 
-	if err != nil {
-		return err
-	}
-
 	return err
 }
 
@@ -71,4 +67,56 @@ func (api *UltipaAPI) DownloadAllAlgoResultFile(taskId string, config *configura
 	}
 
 	return nil
+}
+
+func (api *UltipaAPI) DownloadAllAlgoResultFile2(taskId string, config *configuration.RequestConfig, receive func(data []byte, fileName string) error) error {
+	var err error
+
+	client, err := api.GetControlClient(config)
+
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel, err := api.Pool.NewContext(config)
+	if err != nil {
+		return err
+	}
+	defer cancel()
+
+	tasks, err := api.ShowTask(taskId, structs.TaskstatusAll, config)
+	if err != nil {
+		return errors.New("get task failed, " + err.Error())
+	}
+
+	files, err := tasks[0].GetTaskFileName()
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		resp, err := client.DownloadFileV2(ctx, &ultipa.DownloadFileRequestV2{
+			FileName: file,
+			TaskId:   taskId,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		for {
+			record, err := resp.Recv()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return err
+			}
+			err = receive(record.Chunk, file)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return err
 }
--- test/download_test.go
+++ test/download_test.go
@@ -1,23 +1,52 @@
 package test
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"testing"
 )
 
 func TestDownload(t *testing.T) {
-	receive := func(data []byte) error {
-		fmt.Printf("%s\n", data)
-		return nil
-	}
-	fileName := "aggregations"
-	file, err := os.Create(fileName)
-	defer file.Close()
+	fileName := "data/ids"
+
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
-	err = client.DownloadAlgoResultFile(fileName, "4", nil, receive)
-	log.Println(err.Error())
+	defer file.Close()
+
+	receive := func(data []byte) error {
+		_, err = file.Write(data)
+
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	err = client.DownloadAlgoResultFile(fileName, "1", nil, receive)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestDownloadAll(t *testing.T) {
+	receive := func(data []byte, fileName string) error {
+
+		file, err := os.OpenFile("./data/"+fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		_, err = file.Write(data)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+	err := client.DownloadAllAlgoResultFile2("1", nil, receive)
+	if err != nil {
+		panic(err)
+	}
 }
