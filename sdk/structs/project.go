6aa598ffc829b091b065358395077dab3e37f030
yujincheng <272167037@qq.com>
2024-09-29 17:51:04 +0800

    add project

---
+++ sdk/structs/project.go
@@ -0,0 +1,11 @@
+package structs
+
+type Project struct {
+	ProjectName string `json:"project_name"`
+	ProjectType string `json:"project_type"`
+	FilterType  string `json:"filter_type"`
+	GraphName   string `json:"graph_name"`
+	Status      string `json:"status"`
+	Stats       string `json:"stats"`
+	Config      string `json:"config"`
+}
