3a87d7e3db81e5dc11e20b1d7c1b6ce40a3b00eb
yujincheng <272167037@qq.com>
2025-06-03 16:39:26 +0800

    fix jira bugs:  alter policy

--- sdk/structs/policy.go
+++ sdk/structs/policy.go
@@ -126,6 +126,25 @@ func (p *Policy) ToCreatePolicyUql() string {
 }
 
 func (p *Policy) ToAlterPolicyUql() string {
+	if p.PropertyPrivileges.Node.Read == nil {
+		p.PropertyPrivileges.Node.Read = [][]string{}
+	}
+	if p.PropertyPrivileges.Node.Write == nil {
+		p.PropertyPrivileges.Node.Write = [][]string{}
+	}
+	if p.PropertyPrivileges.Node.Deny == nil {
+		p.PropertyPrivileges.Node.Deny = [][]string{}
+	}
+
+	if p.PropertyPrivileges.Edge.Read == nil {
+		p.PropertyPrivileges.Edge.Read = [][]string{}
+	}
+	if p.PropertyPrivileges.Edge.Write == nil {
+		p.PropertyPrivileges.Edge.Write = [][]string{}
+	}
+	if p.PropertyPrivileges.Edge.Deny == nil {
+		p.PropertyPrivileges.Edge.Deny = [][]string{}
+	}
 	//return fmt.Sprintf("alter().policy(\"%s\").set({\ngraph_privileges: %s,\nsystem_privileges: %s,\npolicies: %s,\nproperty_privileges: %s\n})", p.Name, utils.ToJSONString(p.GraphPrivileges), utils.ToJSONString(p.SystemPrivileges), utils.ToJSONString(p.AsPolicies), utils.ToJSONString(p.PropertyPrivileges))
 	return fmt.Sprintf("alter().policy(\"%s\").set(%s)", p.Name, utils.ToJSONString(p))
 }
